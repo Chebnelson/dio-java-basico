a926be3b08c645d6c43f19cc0592893affa2ced9
Chebnelson <chebnelson@hotmail.com>
2024-07-07 13:09:38 -0300

    operadores 2

---
+++ Meu_primeiro_projeto_JAVA/src/edu/kleber/operadores/operadores.java
@@ -0,0 +1,58 @@
+package edu.kleber.operadores;
+
+public class operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+    }
+    
+}
+ /* Classificação de operadores
+ 
+ 
+     Atribuição
+
+     Representado pelo simbulo de igualdade =.
+
+     o operador de atribuição é ultilizado para definir o valor inicial ou
+     sobrescrever o valor de uma variavel. Em java definimos um tipo, nome e
+     opcionalmente atribuimos um valor à variavel através do operador de atribuição
+         Ex:
+         1 // classes operadores.java
+         2 String nome = "kleber";
+         3 int idade = 29;
+         4 double peso = 62.5; --------> numero quebrado separado por ponto.
+         5 char sexo = 'M'; -----> somente 1 caractere.
+         6 boolena doadorOrgao = false; ------> "sim" ou "nao"
+         7 Date dataNascimento = new Date();
+
+    Operadores Aritimeticos
+        O operador aritimtico é ultilizado para realizar operações matematicas
+        entre valores numéricos, podendo se tornar ou não uma expressão mais complexa.
+        
+        Os operadores aritimeticos são: + (adição), - (subtração), * (multiplicação)
+        e / (divisão).
+            Ex:
+                1 double soma = 10.5 + 15.7;
+                2 int subtração = 113 - 25;
+                3 int multiplicação = 20 * 7;
+                4 int divisão = 15 / 3;
+                5 inte modulo = 18 % 3; -----> modulo significa o resto da divisão. 
+                6 double resultado = (10 * 7) + ( 20 / 4);
+        O OPERADOR DE ADIÇÃO (+), QUANDO ULTILIZADA EM VARIÁVEIS DO TIPO TEXTO,
+        FAZ A CONCATENAÇÃO DE TEXTO (JUNTA AS PALAVRAS/TEXTOS)   
+            Ex: 
+                String nomeCompleto = "LINGUAGEM" + "JAVA";
+                = LINGUAGEMJAVA
+
+
+  * 
+  */
+
+
+
+
+
+
+

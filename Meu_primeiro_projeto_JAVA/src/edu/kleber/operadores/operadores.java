92d479524ca37657887722110c1d3c7d6e9eb650
Chebnelson <chebnelson@hotmail.com>
2024-07-07 19:22:31 -0300

    Operadore 6

--- Meu_primeiro_projeto_JAVA/src/edu/kleber/operadores/operadores.java
+++ Meu_primeiro_projeto_JAVA/src/edu/kleber/operadores/operadores.java
@@ -4,11 +4,51 @@ public class operadores {
     public static void main(String[] args) {
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
+        
+        // UNÁRIO
+
         int numero = 5;
         System.out.println(- numero);
         System.out.println( numero);
+
+        // TENÁRIO
+
+        int a, b;
+
+            a = 5;
+            b = 6;
+
+            String resultado = a==b ? "verdadeiro" : "false";
+            System.out.println( resultado);
+
+        // RELACIONAIS
+        
+        int numero1 = 2;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println( "numeroUm é igual a numeroDois?" + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println( "numeroUm é diferente ao numeroDois?" + simNao);
+
+        simNao = numero1 >= numero2;
+        System.out.println( "numeroUm é maior ou igual ao numeroDois?" + simNao);
+
+        simNao = numero1 < numero2;
+        System.out.println( "numeroUm é menor ao numeroDois?" + simNao);
+
+        simNao = numero1 < numero2;
+        System.out.println( "numeroUm é menor ou igual ao numeroDois?" + simNao);
+
+        if(numero1 == numero2){
+            
+        System.out.println( "A NOSSA CONDIÇÃO É VERDADEIRA!!");
+        }
+       
+
     }
-    
+
 }
  /* Classificação de operadores
  
@@ -62,7 +102,52 @@ public class operadores {
             (--) Operador unário de decremento de valor - decrementa o valor em 1 unidade;
             (!) Operador unário lógico de negação - nega o valor de uma expressão booleana;
 
-                Ex:
+        
+        Ternário
+        O Operador de Condições Tenária é uma forma resumida para denfinir uma
+        condição e escolhe por um dentre dois valores. Você deve pesar numa
+        condição tenaria como se fosse uma condição IF normal, porem, de uma forma
+        em que toda a sua estrutura estará escrita numa única linha.
+        O operador ternário é representado pelos símbolos ?: ultilizados na 
+        na seguinte estrutura de sintaxe:
+
+            <Expressão condicional> ? <caso condição seja true> :<caso seja false> 
+
+            Ex:
+
+            int a, b:
+
+            a = 5
+            b = 6
+
+            exmplo de condicional ultilizando um estrutura if/else
+
+            if (a==b)
+                resultado = "verdadeiro";
+
+            else
+                resultado = "falso";
+
+            mesma condicional, mas dessa vez, ultilizando o operador
+
+            String resultado = (a==b) ? "verdadeiro" : "false";
+
+        RELACIONAIS
+        Os operadores relacionais avaliam a relação entre duas variáveis ou
+        expressões. Neste caso, mais precisamente, definem se o operador à esquerda
+        é igual, diferente, menor, menor ou igaul, maior, maior ou igual ao da direita,
+        retornando umvalor booleano como resultado.
+
+        == Quando desejamos verificar se uma variavel é IGUAL A outra.
+
+        != Quando desejamoes verificar se uma variável é diferente da outra.
+
+        >= Quando desejamos verificar se uma variável é MAIOR OU IGUAL a outra.
+
+        < Quando desejamos verificar se uma variável é MENOR QUE outra.
+
+        <= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra.
+
 
 
   * 

c554eedf6b5277399b78345db86820c7a5281520
Chebnelson <chebnelson@hotmail.com>
2024-07-07 20:49:30 -0300

    metodo 3

--- Meu_primeiro_projeto_JAVA/src/edu/kleber/metodos/metodos.java
+++ Meu_primeiro_projeto_JAVA/src/edu/kleber/metodos/metodos.java
@@ -3,3 +3,19 @@ package edu.kleber.metodos;
 public class metodos {
     
 }
+/**
+   MÉTODOS
+  Uma classe é definida por atributos e metodos. Já vimos que atributos são,
+  em sua grande maioria, variáveos de diferentes tipos e valores. Os métodos, em sua
+  vez, correspondema a funções ou sub-rotinas disponiveis dentro da nossas classes.
+  
+   Critérios de nomeação de Métodos
+   Esses criterios não são obrigatorios, mas é recomendavel que sejam seguidos, pois
+   essas convenções facilitam a vida dos programadores ao trabalharem em codigos de
+   forma colaboratima.
+  
+      + Deve ser nomeado como verbo
+      + Seguir o padrao camelCase(TODAS AS LETRAS MINUSCULAS COM EXCEÇÃO DA 
+  PRIMEIRA LETRA DA SEGUNDA PALAVRA.)
+ 
+*/

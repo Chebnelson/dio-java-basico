f9e44d6c747be3e50ccf97a24bb5d2a7496b503d
Chebnelson <chebnelson@hotmail.com>
2024-07-14 21:17:03 -0300

    palavras reservadas

---
+++ Meu_primeiro_projeto_JAVA/src/edu/kleber/PalavraReservadas/PalavrasReservadas.java
@@ -0,0 +1,10 @@
+package edu.kleber.PalavraReservadas;
+
+public class PalavrasReservadas {
+    
+}
+
+/* PALAVRAS RESERVADAS
+São 52 palavras reservadas pela linguagem.
+As palavras são escritas em minusculo.
+*/
